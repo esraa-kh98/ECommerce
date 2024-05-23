21bfc891f5385ad0babeca2f8d503fa428f74e56
esraa-kh98 <102542793+esraa-kh98@users.noreply.github.com>
2024-05-23 22:29:41 +0300

    End Controller Part

---
+++ ECommerce/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ECommerce.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly EcommerceDbContext _context;
+        public CategoriesController(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var Response = _context.Categories.ToList();
+            return View(Response);
+        }
+    }
+}
